16a47a4518b96abbff2833bca51b14fa89c2916a
WMY <mengyang1229@gmail.com>
2019-03-11 20:02:59 +0800

    DELETE响应数据不做text处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -134,7 +134,11 @@ export default function request(url, option) {
     .then(response => {
       // DELETE and 204 do not return data by default
       // using .json will report an error.
-      if (newOptions.method === 'DELETE' || response.status === 204) {
+      if (
+        // newOptions.method === 'DELETE'
+        // ||
+        response.status === 204
+      ) {
         return response.text();
       }
       return response.json();
